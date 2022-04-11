98ed896e09380eeff4c885cd6019d823799f8190
Julian <julian_delisio@hotmail.com>
2022-04-11 00:02:18 -0300

    inicia estructura

--- src/componentes/Boton.js
+++ src/componentes/Boton.js
@@ -1,9 +1,15 @@
 import React from 'react';
+import "../hojas-de-estilo/Boton.css";
 
 function Boton(props){
+
+    const esOperador = valor => {
+      return isNaN(valor) && (valor !== ". ") && (valor !== "=");
+    };
+
     return(
         <div
-        className={ `boton-` }
+        className={ `boton-contenedor ${esOperador(props.children) ? "operador" : null}`.trimEnd() }
         >
            {props.children}
         </div>
